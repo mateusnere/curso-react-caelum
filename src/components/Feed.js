70d40af23acf63884148fd80222dfee19b61d57d
react7967 <react7967@caelumBSB01-06>
2018-09-18 20:13:32 -0300

    Apresentando uma lista de usuarios no feed.

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,19 +1,25 @@
 import React, {Component} from 'react';
-import {View, Text, Image, Dimensions} from 'react-native';
+import {View, ScrollView, Text, Image, Dimensions} from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
 export default class Feed extends Component {
     render() {
+        const fotos = [
+            {id: 1, usuario: 'Rafael'},
+            {id: 2, usuario: 'Alberto'},
+            {id: 3, usuario: 'Vitor'}
+        ]
         return (
-            <View>
-                <Text>
-                    Mateus
-                </Text>
-
-                <Image source={require('../../resources/img/alura.png')} 
-                    style={{width: width, height:width}} />
-            </View>
+            <ScrollView>
+                {fotos.map(foto => 
+                    <View key="foto.id">
+                        <Text>{foto.usuario}</Text>
+                        <Image source={require('../../resources/img/alura.png')} 
+                            style={{width: width, height:width}} />
+                    </View>  
+                )}
+            </ScrollView>
         );
     }
 }
