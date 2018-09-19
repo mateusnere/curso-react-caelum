336cb8072374639b30ee66def861d3a4a27a5cbe
react7967 <react7967@caelumBSB01-06>
2018-09-18 22:55:20 -0300

    Implementando likes

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,44 @@
 import React, {Component} from 'react';
-import {View, Text, Image, StyleSheet, Dimensions} from 'react-native';
+import {View, Text, Image, StyleSheet, Dimensions, TouchableOpacity} from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
 export default class Post extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            foto: this.props.foto
+        }
+    }
+
+    carregaIcone(likeada) {
+        return likeada ? require('../../resources/img/s2-checked.png') : 
+        require('../../resources/img/s2-checked.png');
+    }
+
+    
+
     render() {
+
+        const { foto } = this.state;
+
         return(
+            
             <View>
                 <View style={styles.cabecalho}>
-                    <Image source = {{uri: this.props.foto.urlPerfil}}
+                    <Image source = {{uri: foto.urlPerfil}}
                         style = {styles.fotoDePerfil} />
-                    <Text>{this.props.foto.loginUsuario}</Text>
+                    <Text>{foto.loginUsuario}</Text>
                 </View>
-                <Image source = {{uri: this.props.foto.urlFoto}}
+                <Image source = {{uri: foto.urlFoto}}
                         style = {styles.foto} />
+                <View style={styles.rodape}>
+                    <TouchableOpacity onPress={() => console.warn('Botao funcionando!')}>
+                        <Image style={styles.botaoDeLike} 
+                            source={require('../../resources/img/s2.png')} />
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -35,5 +59,12 @@ const styles = StyleSheet.create({
     foto: {
         width: width,
         height: width,
+    },
+    rodape: {
+        margin: 10,
+    },
+    botaoDeLike: {
+        height: 40,
+        width: 40,
     },
 });
