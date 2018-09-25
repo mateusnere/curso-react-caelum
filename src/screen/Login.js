c430d3c4b06af8d93b7c96486c5e1439fa6c87d1
react7967 <react7967@caelumBSB01-06>
2018-09-24 22:09:41 -0300

    Enviando dados para autenticação.

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -12,22 +12,56 @@ const width = Dimensions.get('screen').width;
 
 export default class Login extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            usuario: '',
+            senha: '',
+        }
+    }
+
+    efetuaLogin = () => {
+        const uri = 'https://instalura-api.herokuapp.com/api/public/login';
+
+        const requestInfo = {
+            method: 'POST',
+            body: JSON.stringify({
+                login: this.state.usuario,
+                senha: this.state.senha
+            }),
+            headers: new Headers({
+                'Content-type': 'application/json'
+            })
+        }
+
+        fetch(uri, requestInfo).then(response => {
+            if(response.ok) {
+                return response.text();
+            } else {
+                throw new Error('Não foi possível efetuar login');
+            }
+        }).then(token => console.warn(token));
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.titulo}>Instalura</Text>
                 <View style={styles.form}>
                     <TextInput 
+                        autoCapitalize='none'
                         style={styles.input}
                         placeholder="Usuário..."
                         onChangeText={texto => this.setState({usuario: texto})} />
                     
                     <TextInput 
+                        autoCapitalize='none'
+                        secureTextEntry={true}
                         style={styles.input}
                         placeholder="Senha..."
                         onChangeText={texto => this.setState({senha: texto})} />
 
-                    <Button title='Login' onPress={() => console.warn('Login')} />
+                    <Button title='Login' onPress={this.efetuaLogin} />
                 </View>
             </View>
         );
