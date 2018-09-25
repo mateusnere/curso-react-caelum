bbe50957149fd111ac64c6ae9b0d47a6adc12839
react7967 <react7967@caelumBSB01-06>
2018-09-24 21:43:19 -0300

    Criando tela de login do instalura

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import {AppRegistry} from 'react-native';
 import Feed from './src/components/Feed';
+import Login from './src/screen/Login';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Feed);
+AppRegistry.registerComponent(appName, () => Login);
